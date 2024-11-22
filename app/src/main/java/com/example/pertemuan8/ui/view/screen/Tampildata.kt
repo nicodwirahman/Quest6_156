d75c4f248c3d8d42b4e1b258853a0d88a47a8b1b
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-11-22 12:07:26 +0700

    Menyusun layout dengan padding dan latar putih.

--- app/src/main/java/com/example/pertemuan8/ui/view/screen/Tampildata.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/Tampildata.kt
@@ -3,11 +3,14 @@ package com.example.pertemuan8.ui.view.screen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.pertemuan8.R
 import com.example.pertemuan8.model.Mahasiswa
 
@@ -26,6 +29,12 @@ fun TampilView(
             .background(colorResource(id = R.color.primary)),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
 }
 
 }
