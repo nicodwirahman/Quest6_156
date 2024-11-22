80cffd01ab8f02a79b3b6ffe8dd4d59c9fc93054
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-11-22 12:17:05 +0700

    Memberi jarak dengan padding 16dp.

--- app/src/main/java/com/example/pertemuan8/ui/view/screen/Tampildata.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/Tampildata.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -69,6 +70,7 @@ fun TampilView(
                             fontWeight = FontWeight.Bold,
                             fontSize = 14.sp
                         )
+                        Spacer(modifier = Modifier.padding(16.dp))
 
                 }
 
