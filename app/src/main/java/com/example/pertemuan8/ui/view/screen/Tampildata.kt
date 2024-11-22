940bc39feb95f8fe22257f99e9fca01eadfc22d5
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-11-22 12:04:10 +0700

    Menyusun layout dengan background warna utama.

--- app/src/main/java/com/example/pertemuan8/ui/view/screen/Tampildata.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/Tampildata.kt
@@ -1,7 +1,14 @@
 package com.example.pertemuan8.ui.view.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.pertemuan8.R
 import com.example.pertemuan8.model.Mahasiswa
 
 //@Preview(showBackground = true)
@@ -13,6 +20,12 @@ fun TampilView(
     onResetButtonClicked: () -> Unit,
 
     ) {
-
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(id = R.color.primary)),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+}
 
 }
