c27ff38421e90d1432020d03852a7aea1631a82b
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-11-22 12:11:41 +0700

    Menampilkan teks "Nim" dengan format tebal.

--- app/src/main/java/com/example/pertemuan8/ui/view/screen/Tampildata.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/Tampildata.kt
@@ -7,13 +7,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.pertemuan8.R
 import com.example.pertemuan8.model.Mahasiswa
 
@@ -42,6 +45,16 @@ fun TampilView(
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween
+            ){
+                Column {
+                    Text(
+                        text = "Nim:",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 14.sp
+                    )
+                }
+
+            }
         }
 }
 
