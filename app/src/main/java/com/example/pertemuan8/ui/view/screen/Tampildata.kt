60b2fe5ca24a8324a719a18495d6f1180d5d8c2d
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-11-22 11:59:38 +0700

    `onbackbuttonClicked` menangani aksi tombol kembali, `onResetButtonClicked` untuk reset form.

---
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/Tampildata.kt
@@ -0,0 +1,18 @@
+package com.example.pertemuan8.ui.view.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pertemuan8.model.Mahasiswa
+
+//@Preview(showBackground = true)
+
+@Composable
+fun TampilView(
+    uistate: Mahasiswa,
+    onbackbuttonClicked: () -> Unit,
+    onResetButtonClicked: () -> Unit,
+
+    ) {
+
+
+}
