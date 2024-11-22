bc05d71d9748d7383a892f91cbc3f31149e37d71
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-11-22 15:32:04 +0700

    mengubah dan menambah codingan yang salah

--- app/src/main/java/com/example/pertemuan8/ui/view/screen/Tampildata.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/Tampildata.kt
@@ -1,5 +1,6 @@
-package com.example.pertemuan8.ui.view.screen
+package com.pam.quest6_139.ui.view.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,12 +9,19 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,69 +29,111 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.pertemuan8.R
 import com.example.pertemuan8.model.Mahasiswa
+import com.example.pertemuan8.model.RencanaStudi
+
 
 //@Preview(showBackground = true)
-
 @Composable
-fun TampilView(
-    uistate: Mahasiswa,
+fun Tampildata(
+    mahasiswa: Mahasiswa,
+    krs: RencanaStudi,
     onbackbuttonClicked: () -> Unit,
     onResetButtonClicked: () -> Unit,
-
-    ) {
+) {
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(colorResource(id = R.color.primary)),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Spacer(modifier = Modifier.padding(top = 16.dp))
+        Row(
+            modifier = Modifier.padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.umy),
+                contentDescription = "umy",
+                modifier = Modifier.size(45.dp)
+            )
+            Spacer(modifier = Modifier.padding(start = 16.dp))
+            Column {
+                Text(
+                    text = "Data KRS Mahasiswa",
+                    color = Color.White,
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "Universitas Muhammadiyah Yogyakarta",
+                    color = Color.White,
+                    fontWeight = FontWeight.Light
+                )
+            }
+        }
+
+
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .background(Color.White)
                 .padding(16.dp),
-           horizontalAlignment = Alignment.CenterHorizontally
-        ){
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween
-            ){
+            ) {
                 Column {
                     Text(
                         text = "Nim:",
-                        fontWeight = FontWeight.Bold,
+                        fontWeight = androidx.compose.ui.text.font.FontWeight.Bold,
                         fontSize = 14.sp
                     )
                     Text(
-                        text = uistate.nim,
-                        fontStyle = FontStyle.Italic,
+                        text = mahasiswa.nim,
+                        fontStyle = androidx.compose.ui.text.font.FontStyle.Italic,
                         fontSize = 14.sp
                     )
                     Text(
-                        text = uistate.nama,
-                        fontStyle = FontStyle.Italic, // Gunakan FontStyle langsung
+                        text = "Nama:",
+                        fontWeight = androidx.compose.ui.text.font.FontWeight.Bold,
                         fontSize = 14.sp
                     )
-                    Column {
-                        Text(
-                            text = uistate.email,
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 14.sp
-                        )
-                        Spacer(modifier = Modifier.padding(16.dp))
-                        Column(
-                            modifier = Modifier.fillMaxWidth()
-                        ) {
-                            Text(
-                                text = "Matakuliah yang diambil:",
-                                fontWeight = FontWeight.Bold,
-                                fontSize = 14.sp
-                        }
+                    Text(
+                        text = mahasiswa.nama,
+                        fontStyle = androidx.compose.ui.text.font.FontStyle.Italic,
+                        fontSize = 14.sp
+                    )
 
+
+                    Text(
+                        text = "Email:",
+                        fontWeight = androidx.compose.ui.text.font.FontWeight.Bold,
+                        fontSize = 14.sp
+                    )
+
+
+                    Text(
+                        text = mahasiswa.email,
+                        fontWeight = androidx.compose.ui.text.font.FontWeight.Bold,
+                        fontSize = 14.sp
+                    )
                 }
+            }
+            Spacer(modifier = Modifier.padding(16.dp))
+            Column(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "Matakuliah yang diambil:",
+                    fontWeight = androidx.compose.ui.text.font.FontWeight.Bold,
+                    fontSize = 14.sp
+                )
+
 
             }
         }
-}
+    }
 
 }
